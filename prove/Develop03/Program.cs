67d9dae09bf57a0b33ac0b32088bab6bfc87e8c2
sapphireflowers <sapphireflowers30@gmail.com>
2026-02-23 09:34:34 -0700

    cse

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Journal myJournal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+                string prompt = promptGenerator.Prompt();
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+                string userResponse = Console.ReadLine();
+
+                Console.Write("Press enter to continue or type 'quit' to finish: ");
+                string mood = Console.ReadLine();
     }
 }
